b4027a424502b5881d80a73177275d11b2b3600a
Paulin <koutoatipaulin@gmail.com>
2025-08-08 22:54:04 +0000

    day_17

---
+++ day_17/Try_except.py
@@ -0,0 +1,6 @@
+#unpacking a list
+names = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland', 'Estonia', 'Russia']
+*nordic_countries , es, ru = names
+print("nordic_countries:", nordic_countries)
+print("es:", es)
+print("ru:", ru)
